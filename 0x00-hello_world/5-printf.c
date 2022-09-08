263f2148be106d0dcf3fa60671e7c69589a5f071
FortuneGTL <dortunatusazobua@gmail.com>
2022-09-08 16:41:58 -0700

    Done with the mandatory tasks

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main- Serves as the entry point for the program
+ * main-entry point for the program.
  *
- * Return: By default returns zero if no error otherwise returns a non zero value
+ * Return:0 if no errors, return non zero if errors
  */
 int main(void)
-{ 
-printf("with proper grammer, but the outcome is a piece of art,\n");
-return (0);
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
