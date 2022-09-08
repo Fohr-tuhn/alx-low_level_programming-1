526cbb4530254ea614cbf80316f5a75f3fbb97cb
FortuneGTL <107363656+FortuneGTL@users.noreply.github.com>
2022-09-08 23:33:55 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main- Serves as the entry point for the program
+ * main- main block
  *
- * Return: By default returns zero if no error otherwise returns a non zero value
- */
+ * Return: 0
+ *
 int main(void)
 { 
 puts("\"Programming is like building a multilingual puzzle");
